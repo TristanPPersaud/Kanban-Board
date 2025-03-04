ed0f524c9bd68f1f0cd08ff012aeca8408e7041b
Tristan Persaud <tristanppersaud@gmail.com>
2025-03-03 19:40:27 -0600

    debugging routes

--- Develop 14/server/src/routes/index.ts
+++ Develop 14/server/src/routes/index.ts
@@ -5,7 +5,9 @@ import { authenticateToken } from '../middleware/auth.js';
 
 const router = Router();
 
+console.log("Registering /auth routes...");
 router.use('/auth', authRoutes);
+console.log("Auth routes registered:", authRoutes);
 // TODO: Add authentication to the API routes
 router.use('/api', authenticateToken, apiRoutes);
 
--- Develop 14/server/src/server.ts
+++ Develop 14/server/src/server.ts
@@ -5,15 +5,24 @@ dotenv.config();
 import express from 'express';
 import routes from './routes/index.js';
 import { sequelize } from './models/index.js';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Serves static files in the entire client's dist folder
 app.use(express.static('../client/dist'));
-
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
 app.use(routes);
+app._router.stack.forEach((r: any) => {
+  if (r.route) console.log(`Registered route: ${r.route.path}`);
+  else if (r.name === 'router') {
+    r.handle.stack.forEach((subRoute: any) => {
+      if (subRoute.route) console.log(`Registered sub-route: ${subRoute.route.path}`);
+    });
+  }
+});
 
 sequelize.sync({force: forceDatabaseRefresh}).then(() => {
   app.listen(PORT, () => {
